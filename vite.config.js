c8672b842ad24b2763e02c5d3011069f037981c4
Smartsantosh1928 <smartsantosh1928@gmail.com>
2023-04-02 06:39:30 +0530

    vite configed

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react'
 import googleFonts from 'google-fonts-webpack-plugin';
 
 export default defineConfig({
-  plugins: [react(),
-    googleFonts({
-      fonts: [
-        { family: 'Mulish', variants: ['400', '500', '600'] }
-      ]
-    })
-],
-build: {
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        'google-fonts': ['google-fonts']
-      }
-    }
-  }
-}
+  plugins: [react()]
 })
